f9f15e6085535f3a1222a7516b92f22d9fbbdd55
chickfilaman51 <krishbehera10@gmail.com>
2024-05-18 16:20:45 -0400

    Update RecentProjects component and fixed form

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,24 +7,17 @@ import { PinContainer } from "./ui/Pin";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20 my ">
       <h1 className="heading">
         Our project's{" "}
-        <span className="text-purple">components</span>
+        <span className="text-purple my-4">components</span>
       </h1>
       <h1 className="heading">
         
       </h1>
-  <div className="form-container">
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSdCX3ZcrrsdomOTXLcnphzurhDrb_29ZKeW2Z74L5Qgn_UANg/viewform?embedded=true"
-      width="640"
-      height="800"
-      title="Google Form"
-    >
-      Loading…
-    </iframe>
-  </div>
+      <div className="form-container flex justify-center">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdCX3ZcrrsdomOTXLcnphzurhDrb_29ZKeW2Z74L5Qgn_UANg/viewform?embedded=true" width="640" height="787">Loading…</iframe>
+</div>
     </div>
   );
 };
