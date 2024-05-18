bfb43022e8f564bc9d9a2b40222df21936aceaf6
Grumb0t <aniccdu34@gmail.com>
2024-05-18 16:53:18 -0400

    form

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,23 +1,27 @@
 "use client";
 
 import { FaLocationArrow } from "react-icons/fa6";
-
 import { projects } from "@/data";
 import { PinContainer } from "./ui/Pin";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20 my ">
-      <h1 className="heading">
-        Our project's{" "}
-        <span className="text-purple my-4">components</span>
-      </h1>
-      <h1 className="heading">
-        
-      </h1>
-      <div className="form-container flex justify-center">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdCX3ZcrrsdomOTXLcnphzurhDrb_29ZKeW2Z74L5Qgn_UANg/viewform?embedded=true" width="640" height="787">Loading…</iframe>
-</div>
+    <div>
+      <div className="py-20 my">
+        <h1 className="heading">
+          Analyze your{" "}
+          <span className="text-purple my-4">conversation</span>
+        </h1>
+      </div>
+      <div className="form-container flex justify-center items-center mt-10">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdCX3ZcrrsdomOTXLcnphzurhDrb_29ZKeW2Z74L5Qgn_UANg/viewform?embedded=true"
+          width="640"
+          height="787"
+        >
+          Loading…
+        </iframe>
+      </div>
     </div>
   );
 };
