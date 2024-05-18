b800490e16c6629a80b793f28a431dfdf6b82133
Grumb0t <aniccdu34@gmail.com>
2024-05-18 15:31:14 -0400

    images

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["JavaScript", "React", "HTML"];
+  const rightLists = ["CSS", "HTML", "React"];
 
   const [copied, setCopied] = useState(false);
 
--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
   { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
+  { name: "Submit a Conversation", link: "#projects" },
+  { name: "Examples", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
 ];
 
