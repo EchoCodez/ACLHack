c410d0e5089981ded658fcf1d5f129cade9e4d97
Grumb0t <aniccdu34@gmail.com>
2024-05-18 15:14:31 -0400

    ensemble machine learning highlight *sparkle*

--- data/index.ts
+++ data/index.ts
@@ -28,7 +28,7 @@ export const gridItems = [
   },
   {
     id: 3,
-    title: "languages",
+    title: "coding languages",
     description: "We used many",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
@@ -49,8 +49,8 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
-    description: "The Inside Scoop",
+    title: "Ensemble Machine Learning",
+    description: "To make this happen, we used",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
