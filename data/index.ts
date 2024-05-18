12b0553facc3a22df2dfd0213b2df31422a3bd4a
Grumb0t <aniccdu34@gmail.com>
2024-05-18 15:17:51 -0400

    images

--- data/index.ts
+++ data/index.ts
@@ -43,7 +43,7 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "",
+    img: "/grid.svg",
     spareImg: "/b4.svg",
   },
 
@@ -55,7 +55,7 @@ export const gridItems = [
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
     img: "/b5.svg",
-    spareImg: "/grid.svg",
+    spareImg: "/machineLeaning.jpeg",
   },
   {
     id: 6,
