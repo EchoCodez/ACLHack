3b93752405a90cbcea5d8857cd66f2bd42a06d98
EchoCodez <mihir.nimkar@gmail.com>
2024-05-18 20:06:44 -0400

    Changed transcript handling

--- backend/result.py
+++ backend/result.py
@@ -1,5 +1,6 @@
 from processing import classify_message
 import gspread as gs
+import time
 
 gc = gs.service_account(filename='backend/key.json')
 sh1 = gc.open_by_url('https://docs.google.com/spreadsheets/d/1WkQRziqNg-2dFLd-EDaGIFzWWf8mN6v1lRD9u4JNoWQ/edit?usp=sharing')
@@ -7,9 +8,16 @@ sh1 = gc.open_by_url('https://docs.google.com/spreadsheets/d/1WkQRziqNg-2dFLd-ED
 ws1 = sh1.worksheet('Form Responses 1')
 
 def get_data():
-    for i in ws1.get_all_records():
-        result = classify_message(i.get("Put the transcript of your conversation here"))
-        print(f'The message "{i.get("Put the transcript of your conversation here")}" is classified as: {result}')
+    for i in str(ws1.get_all_records()[-1].get("Put the transcript of your conversation here")).split("."):
+        result = classify_message(i)
+        if result == "Fraudulent":
+            print(f'The message "{i}" is classified as: {result}')
 
-get_data()
+thing = ws1.get_all_records()[-1].get("Put the transcript of your conversation here")
+
+while True:
+    time.sleep(1)
+    if ws1.get_all_records()[-1].get("Put the transcript of your conversation here") != thing:
+        get_data()
+        thing = ws1.get_all_records()[-1].get("Put the transcript of your conversation here")
 
