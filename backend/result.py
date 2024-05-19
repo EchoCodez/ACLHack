3bee1d846c4dd9af16056cd8ef5e6e93b20a7ebe
EchoCodez <mihir.nimkar@gmail.com>
2024-05-18 22:37:04 -0400

    change github conflicts

--- backend/result.py
+++ backend/result.py
@@ -1,23 +1,27 @@
 from processing import classify_message
 import gspread as gs
 import time
+from send_email import send_email
 
 gc = gs.service_account(filename='backend/key.json')
 sh1 = gc.open_by_url('https://docs.google.com/spreadsheets/d/1WkQRziqNg-2dFLd-EDaGIFzWWf8mN6v1lRD9u4JNoWQ/edit?usp=sharing')
 
 ws1 = sh1.worksheet('Form Responses 1')
 
-def get_data():
+def get_data(body):
     for i in str(ws1.get_all_records()[-1].get("Put the transcript of your conversation here")).split("."):
         result = classify_message(i)
         if result == "Fraudulent":
-            print(f'The message "{i}" is classified as: {result}')
+            body += (f'The message "{i}" is classified as: {result}\n')
+    return body
 
 thing = ws1.get_all_records()[-1].get("Put the transcript of your conversation here")
 
 while True:
+    body = ""
     time.sleep(1)
     if ws1.get_all_records()[-1].get("Put the transcript of your conversation here") != thing:
-        get_data()
+        full = get_data(body)
+        send_email("Fraudulent Conversation Notice", full, ws1.get_all_records()[-1].get("Enter your email address. You will receive the analyzation report at this email."))
         thing = ws1.get_all_records()[-1].get("Put the transcript of your conversation here")
 
---
+++ backend/send_email.py
@@ -0,0 +1,62 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import base64
+import os.path
+import pickle
+
+# If modifying these SCOPES, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+
+    Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+    Returns:
+    An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    msg = MIMEText(message_text)
+    message.attach(msg)
+
+    raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+    return {'raw': raw_message}
+
+
+def send_email(subject, body, to):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'backend/secrets.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    message = create_message('me', to, subject, body)
+    message = service.users().messages().send(userId='me', body=message).execute()
+
+    print('Message Id: %s' % message['id'])
